ad5b40cc37a71237bc472468833d46a13603177b
Joshua Ordaz <joshuaordaz@comcast.net>
2021-02-07 23:03:31 -0800

    fix path for routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+
+const apiRoutes = require("routes/apiRoutes");
+const htmlRoutes = require("routes/htmlRoutes");
 
 const app = express();
 const PORT = process.env.PORT || "3000";
