e37ad9dbc8a904c826f34ed2deb420816dfb95cd
Joshua Ordaz <joshuaordaz@comcast.net>
2021-02-07 22:53:41 -0800

    update to use router

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,44 +1,37 @@
 const fs = require('fs')
 const noteData = require('../db/db.json')
-const app = require("express").Router();
+const router = require("express").Router();
 
+router.get("/notes", function (req, res) {
+    res.json(noteData);
+});
 
-module.exports = function (app) {
-
-    app.get("/notes", function (req, res) {
-        res.json(noteData);
-    });
-
-    app.post("/notes", function (req, res) {
-
-        if (noteData.length === 0){
-            req.body.id = 0
-        } else {
-            req.body.id = noteData.length
-        }
-
-        noteData.push(req.body)
-        updateDb(noteData)
-        res.json(req.body)
-    })
-
-    function updateDb(notes) {
-
-        notes = JSON.stringify(notes)
-        fs.writeFileSync('db/db.json', notes, err => {
-            if (err) throw err
-        })
+router.post("/notes", function (req, res) {
+    if (noteData.length === 0){
+        req.body.id = 0
+    } else {
+        req.body.id = noteData.length
     }
+    noteData.push(req.body)
+    updateDb(noteData)
+    res.json(req.body)
+})
 
-    app.delete("/notes/:id", function(req, res) {
-
-        for(var i = 0; i < noteData.length; i++){
-            if(noteData[i].id === parseInt(req.params.id)){
-                noteData.splice(i,1)
-                updateDb(noteData)
-            }
-        }
-
-        return res.send();
+function updateDb(notes) {
+    notes = JSON.stringify(notes)
+    fs.writeFileSync('db/db.json', notes, err => {
+        if (err) throw err
     })
 }
+
+router.delete("/notes/:id", function(req, res) {
+    for(var i = 0; i < noteData.length; i++){
+        if(noteData[i].id === parseInt(req.params.id)){
+            noteData.splice(i,1)
+            updateDb(noteData)
+        }
+    }
+    return res.send();
+})
+
+module.exports = router;
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,14 +1,12 @@
 const path = require("path");
-const app = require("express").Router();
+const router = require("express").Router();
 
-module.exports = function(app) {
-    
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
+router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-    }
+module.exports = router;
