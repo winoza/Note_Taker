10186ce61693ebae083882f736af71325b2d4613
Joshua Ordaz <joshuaordaz@comcast.net>
2021-02-07 19:45:43 -0800

    fix port

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("/routes/apiRoutes")(app);
+require("/routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
